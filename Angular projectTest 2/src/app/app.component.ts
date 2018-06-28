47b7598c9fbe6177d1c4342b0e80888bd370f78e
ASIF <ASIF@DESKTOP-BTC4I5S>
2018-06-28 15:09:29 +0530

    first commit
    first commit till angular part 4

---
+++ Angular projectTest 2/src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'my-app',
+    template: `<h1>hello {{name}}</h1>`
+})
+
+export class AppComponent
+{
+    name: string = "Aquib";
+}
