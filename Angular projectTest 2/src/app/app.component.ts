fee90818ee90ced6c89f3d1355da0d6f10963424
ASIF <ASIF@DESKTOP-BTC4I5S>
2018-06-28 18:06:15 +0530

    second commit

--- Angular projectTest 2/src/app/app.component.ts
+++ Angular projectTest 2/src/app/app.component.ts
@@ -2,10 +2,10 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'my-app',
-    template: `<h1>hello {{name}}</h1>`
+    templateUrl:'app/app.component.html'
 })
 
 export class AppComponent
 {
-    name: string = "Aquib";
+    name: string = "Aquib1";
 }
