878140eb7f4feef20b98a72b1de0dbc676793b72
ASIF <ASIF@DESKTOP-BTC4I5S>
2018-06-28 20:28:11 +0530

    Revert "compleyted style css"
    This reverts commit adaecc193b28ca4ebb097450fe8f85ae4c0b7908.

--- Angular projectTest 2/src/app/app.component.js
+++ Angular projectTest 2/src/app/app.component.js
@@ -9,7 +9,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 var core_1 = require("@angular/core");
 var AppComponent = /** @class */ (function () {
     function AppComponent() {
-        this.pageHeader = "Employee details";
+        this.name = "Aquib1";
     }
     AppComponent = __decorate([
         core_1.Component({
--- Angular projectTest 2/src/app/app.module.ts
+++ Angular projectTest 2/src/app/app.module.ts
@@ -1,13 +1,11 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
-import { EmployeeDetails } from './employee/employee.componentnt';
-
+import { AppComponent }  from './app.component';
 
 @NgModule({
   imports:      [ BrowserModule ],
-  declarations: [ AppComponent ,EmployeeDetails],
+  declarations: [ AppComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
